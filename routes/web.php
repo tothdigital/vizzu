5054dac8e9572e258f4ab7b48efca2ff1a163bdc
Toth Digital <paulo.henrique@windowslive.com>
2021-07-13 03:34:31 -0300

    update rotas

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get('/site', function () {}
+    //return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
