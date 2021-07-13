04ee4225af1d9d2d2f4b3305c173142a267f3f7f
Toth Digital <paulo.henrique@windowslive.com>
2021-07-13 03:49:48 -0300

    rota padrao para site

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    Route::get('/site', function () {}
-    //return view('welcome');
-});
+Route::redirect('/', '/site', 301);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
