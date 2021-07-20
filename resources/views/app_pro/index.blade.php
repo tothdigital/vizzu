b31ae2ec8b3e967b6266a876e129841282db9152
Toth Digital <paulo.henrique@windowslive.com>
2021-07-20 01:06:27 -0300

    adsfa

--- resources/views/app_pro/index.blade.php
+++ resources/views/app_pro/index.blade.php
@@ -1,15 +1,67 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('projects.create') }}" title="Create a project"> <i class="fas fa-plus-circle"></i>
+                    </a>
             </div>
         </div>
     </div>
-</x-app-layout>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered table-responsive-lg">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Introduction</th>
+            <th>Location</th>
+            <th>Cost</th>
+            <th>Date Created</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($projects as $project)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $project->name }}</td>
+                <td>{{ $project->introduction }}</td>
+                <td>{{ $project->location }}</td>
+                <td>{{ $project->cost }}</td>
+                <td>{{ date_format($project->created_at, 'jS M Y') }}</td>
+                <td>
+                    <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
+
+                        <a href="{{ route('projects.show', $project->id) }}" title="show">
+                            <i class="fas fa-eye text-success  fa-lg"></i>
+                        </a>
+
+                        <a href="{{ route('projects.edit', $project->id) }}">
+                            <i class="fas fa-edit  fa-lg"></i>
+
+                        </a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                            <i class="fas fa-trash fa-lg text-danger"></i>
+
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+    {!! $projects->links() !!}
+
+@endsection
